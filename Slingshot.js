3593e8c166c5c4b2e155c7773c3cc01f36d8b9e8
ananya-anandhan <smitha.mcc@gmail.com>
2021-01-16 13:22:59 +0530

    pig's vanish

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.05,
             length: 10
         }
 
@@ -14,6 +14,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+    }
+    attach(body){
+      this.sling.bodyA = body;
     }
 
     fly(){
@@ -36,7 +39,7 @@ class SlingShot{
                 image(this.sling3,pointA.x - 30,pointA.y - 10,15,30);
             }
             else{
-                strokeWeight(3);
+                strokeWeight(4);
                 line(pointA.x + 20,pointA.y,pointB.x + 30,pointB.y - 3);
                 line(pointA.x + 20,pointA.y,pointB.x - 10,pointB.y);
                 image(this.sling3,pointA.x + 25,pointA.y - 10,15,30);
